149a9e2401ec2278b44154b7234d2a8608469e31
Rafael <rafael_rampineli@hotmail.com>
2021-06-09 16:54:15 -0300

    Quorum WSFC Start Force

---
+++ PowerShell/Force_Quorum_WSFC_Start.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module FailoverClusters  
+  
+$node = "ServerName"  
+Stop-ClusterNode -Name $node  
+Start-ClusterNode -Name $node -FixQuorum  
+  
+(Get-ClusterNode $node).NodeWeight = 1  
+  
+$nodes = Get-ClusterNode -Cluster $node  
+$nodes | Format-Table -property NodeName, State, NodeWeight  
