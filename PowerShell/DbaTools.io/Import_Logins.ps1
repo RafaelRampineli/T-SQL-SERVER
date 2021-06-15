1095109aa2471f73862057c9d649323de3f878e3
Rafael <rafael_rampineli@hotmail.com>
2021-06-15 17:30:41 -0300

    Script para importar Logins utilizando a ferramenta DbaTools

---
+++ PowerShell/DbaTools.io/Import_Logins.ps1
@@ -0,0 +1,31 @@
+﻿$SourceInstance = 'localhost\MSSQLSERVER'
+$DestinationInstance = 'localhost\MSSQLSERVER'
+
+$LoginsSource =      Get-DbaLogin -SqlInstance $SourceInstance
+$LoginsDestination = Get-DbaLogin -SqlInstance $DestinationInstance
+     
+$diff = $LoginsSource | Where-Object Name -notin ($LoginsDestination.Name)
+
+$diff.Name
+
+if($diff) {
+    Copy-DbaLogin -Source $SourceInstance -Destination $DestinationInstance -Login $diff.Name
+}
+
+
+<# APÓS REALIZAR A IMPORTAÇÃO ENTRE AS INSTÂNCIAS, REALIZAR A VALIDAÇÃO DO SID ATRAVÉS DO SELECT ABAIXO (SERÃO IGUAIS)
+
+select sp.name as login,
+	   sp.sid,
+       sp.type_desc as login_type,
+       sp.create_date,
+       sp.modify_date,
+       case when sp.is_disabled = 1 then 'Disabled'
+            else 'Enabled' end as status
+from sys.server_principals sp
+where sp.type_desc = 'SQL_LOGIN'
+order by sp.name;
+#>  
+    
+
+    
---
+++ PowerShell/DbaTools.io/Import_Logins_AG.ps1
@@ -0,0 +1,16 @@
+﻿$AGLSN = 'AG-Name'
+ 
+$primaryReplica =    Get-DbaAgReplica -SqlInstance $AGLSN | Where Role -eq Primary
+$secondaryReplicas = Get-DbaAgReplica -SqlInstance $AGLSN | Where Role -eq Secondary
+ 
+
+$primaryReplica 
+     
+$LoginsOnPrimary = (Get-DbaLogin -SqlInstance $primaryReplica.Name)
+     
+$secondaryReplicas | ForEach-Object {
+        
+    $LoginsOnSecondary = (Get-DbaLogin -SqlInstance $_.Name)
+     
+    $diff = $LoginsOnPrimary | Where-Object Name -notin ($LoginsOnSecondary.Name) 
+}  
